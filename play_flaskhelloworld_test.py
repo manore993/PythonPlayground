a085c49a7070b4768d6e45c0490064cd9f0546c9
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-27 11:33:46 +0200

    HTML escaping Routing

--- play_flaskhelloworld_test.py
+++ play_flaskhelloworld_test.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from markupsafe import escape
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/hello")
+def method_x():
+    return 'Hello, World'
+
+@app.route("/<name>")
+def hello_someone(name):
+    return f"Hello, {escape(name)}!"
