fdcaa1c8b195f6866d361ac9e79b3b0ff717c1a5
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-07-08 10:00:29 +0200

    Login and clear table try

--- sqllite_try.py
+++ sqllite_try.py
@@ -12,8 +12,17 @@ def create_table():
                 Email TEXT NOT NULL UNIQUE,
                 PASSWORD TEXT NOT NULL
             )"""
+    
+    conn = sql.connect(dbname)
+    try:
+        conn.execute
+    except:
+         pass 
+    conn.close()
+
     with sql.connect(dbname) as conn:
-                conn.execute(query)
+        conn.execute(query)
+        
 
 def register(name, email, password):
         """Register user or return None if fails"""
@@ -38,8 +47,44 @@ def print_table():
     conn.commit()
     print (cur.fetchall())
 
+def login(email, password):
+        """Login user function or return None if fails"""
+        query = "SELECT ID, Name FROM Users WHERE Email=? AND Password=?"
+        print(email)
+        print(password)
+        with sql.connect(dbname) as conn:
+            cur = conn.cursor()
+            cur.execute(
+                query,
+                (
+                    email,
+                    password,
+                ),
+            )
+            rows = list(cur.fetchall())
+            print(len(rows))
+            if len(rows) == 1:
+                print(rows[0])
+                return rows[0]
+            else:
+                return None
+
+def clear():
+    """This function will remove all records"""
+    query = "DELETE FROM Users"
+    
+    with sql.connect(dbname) as conn:
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+        
 create_table()
 
 register("fulu", "fulu@gmail.com", "fulu45")
 
+print_table()
+
+print (login("tulu@gmail.com", "tulu45"))
+
+clear()
 print_table()
