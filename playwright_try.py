3da4bf91c7ae4cd624611b0b49cc6db115169aa6
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-07-16 15:14:33 +0200

    Playwright tried for button and dropdown list.

--- playwright_try.py
+++ playwright_try.py
@@ -1,16 +1,11 @@
 import re
 from playwright.sync_api import sync_playwright
 playwright = sync_playwright().start()
-browser = playwright.chromium.launch(headless=False, slow_mo=100)
+browser = playwright.chromium.launch(headless=False, slow_mo=200)
 
 page =browser.new_page()
 
-
-
 page.goto("http://127.0.0.1:5000")
-
-
-
 
 page.get_by_role("link", name="Register", exact=True).click()
 
@@ -20,5 +15,19 @@ page.get_by_role("textbox", name="email").fill("lulu@gmail.com")
 page.get_by_role("textbox", name="password").fill("lulu4789")
 
 page.get_by_role("button", name="Register").last.click()
+
+
+page.get_by_role("link", name="Login Page").click()
+
+page.get_by_role("textbox", name="email").fill("lulu@gmail.com")
+page.get_by_role("textbox", name="password").fill("lulu4789")
+
+page.get_by_role("button", name="Login").last.click()
+
+page.select_option('select#base',value='10')
+page.get_by_role("textbox", name="input").fill("10")
+page.get_by_role("button", name="Calculate").click()
+
+page.get_by_role("link", name="History").click()
 
 x = input()
