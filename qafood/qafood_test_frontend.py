b8f8e47d9d454c352d9f31f6c35fa3372a0efba9
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-08-26 16:23:11 +0200

    Second test, created csv file for second test, CSV file with toppings passed as a string of toppings, used fixture, added pytest-playwright and playwright in requirements

--- qafood/qafood_test_frontend.py
+++ qafood/qafood_test_frontend.py
@@ -2,13 +2,14 @@ import re
 from playwright.sync_api import Page, expect
 import csv
 import pytest
+import time
 
 address = "https://pprod.aldemia.fr/"
 
-def test_access_homepage(page: Page):
-    page.goto(address)
+# def test_access_homepage(page: Page):
+#     page.goto(address)
 
-    expect(page).to_have_title(re.compile("QAFOOD"))
+#     expect(page).to_have_title(re.compile("QAFOOD"))
 
 # def test_register_username(page: Page):
 #     page.goto(address)
@@ -23,34 +24,63 @@ def test_access_homepage(page: Page):
 
 #     page.get_by_role("button", name="Inscription").last.click()
 #     expect(page.get_by_text("Vous n'avez passé aucune commande")).to_be_visible()
-dataset_test_parameter_de_commande = []
 
-with open('./qafood/test_parametre_de_commande.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-    has_parsed_header = False
-    article_name = 0
-    service_type = 1
-    service_time = 2
+def dataset_test_parametre_de_commande():
+    dataset_test_parameter_de_commande = []
 
-    for row in spamreader:
-        
-        for i in range(0,len(row)):
-            row[i] = row[i].strip()
+    with open('./qafood/test_parametre_de_commande.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+        has_parsed_header = False
+        article_name = 0
+        service_type = 1
+        service_time = 2
 
-        if not has_parsed_header:
-            has_parsed_header = True
-            print (row)
-            article_name = row.index('article_name')
-            service_type = row.index('service_type')
-            service_time = row.index('service_time')
-            continue
+        for row in spamreader:
+            
+            for i in range(0,len(row)):
+                row[i] = row[i].strip()
 
-        #dataset_test_parameter_de_commande += [row]
-        ordered_row = (row[article_name], row[service_type], row[service_time])
-        dataset_test_parameter_de_commande += [ordered_row]
-        
+            if not has_parsed_header:
+                has_parsed_header = True
+                print (row)
+                article_name = row.index('article_name')
+                service_type = row.index('service_type')
+                service_time = row.index('service_time')
+                continue
 
-@pytest.mark.parametrize("article_name, service_type, service_time", dataset_test_parameter_de_commande)
+            #dataset_test_parameter_de_commande += [row]
+            ordered_row = (row[article_name], row[service_type], row[service_time])
+            dataset_test_parameter_de_commande += [ordered_row]
+    return dataset_test_parameter_de_commande
+
+def dataset_test_ajouter_multiple_article_avec_multiple_topping():
+    dataset_test_ajouter_multiple_article_avec_multiple_topping = []
+
+    with open('./qafood/test_ajouter_multiple_article_avec_multiple_topping.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+        has_parsed_header = False
+        article_name = 0
+        topping = 1
+
+        for row in spamreader:
+            
+            for i in range(0,len(row)):
+                row[i] = row[i].strip()
+
+            if not has_parsed_header:
+                has_parsed_header = True
+                print (row)
+                article_name = row.index('article_name')
+                topping = row.index('topping')
+               
+                continue
+
+            #dataset_test_parameter_de_commande += [row]
+            ordered_row = (row[article_name], row[topping])
+            dataset_test_ajouter_multiple_article_avec_multiple_topping += [ordered_row]
+    return dataset_test_ajouter_multiple_article_avec_multiple_topping
+           
+@pytest.mark.parametrize("article_name, service_type, service_time", dataset_test_parametre_de_commande())
 
 def test_parametre_de_commande(page: Page, article_name, service_type, service_time):
     page.goto(address)
@@ -68,8 +98,33 @@ def test_parametre_de_commande(page: Page, article_name, service_type, service_t
 
     page.locator(id_data_service_type).click()
     
-    service_time = "18:30"
 
     page.locator(locator_service_time).select_option(value=service_time)
     page.locator("a.rpress-delivery-opt-update").click()
     page.locator("a.submit-fooditem-button").click()
+
+@pytest.fixture
+def prerequisite_test_ajouter_multiple_article_avec_multiple_topping(page: Page):
+    page.goto(address)
+
+    page.locator("div.rpress-price-holder:not(.rpress-grid-view-holder)").locator('a[data-title="12 wings"]').click() 
+    expect(page.get_by_text("Vos paramètres de commande")).to_be_visible()
+
+    page.locator("#nav-delivery-tab").click()
+    
+    page.locator('#rpress-delivery-hours').select_option(value="17:30")
+    page.locator("a.rpress-delivery-opt-update").click()
+    page.locator("a.submit-fooditem-button").click()
+
+@pytest.mark.parametrize("article_name, topping", dataset_test_ajouter_multiple_article_avec_multiple_topping())
+
+def test_ajouter_multiple_article_avec_multiple_topping(page: Page, prerequisite_test_ajouter_multiple_article_avec_multiple_topping, article_name, topping):
+    page.locator("div.rpress-price-holder:not(.rpress-grid-view-holder)").locator(f'a[data-title="{article_name}"]').click()
+    
+    topping = topping.split(",")
+    for i in range(0, len(topping)):
+        topping[i] = topping[i].strip()
+        page.locator(f'div.food-item-list input[name="{topping[i]}"]').locator('..').check()
+    
+    page.locator("a.submit-fooditem-button").click()
+    
