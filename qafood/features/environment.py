17780d10d8ba68b1e34f47270eab94201ba572b0
Aishwarya MANORE <manore.aishwarya@gmail.com>
2025-02-24 18:20:35 +0100

    Create login fixture to be used in before_all. Select "delivery" and "time" before_feature "Constructing an order".

--- qafood/features/environment.py
+++ qafood/features/environment.py
@@ -8,9 +8,29 @@ def playwright_browser_chrome(context):
     p = sync_playwright().start()
     browser = p.chromium.launch(headless=False, slow_mo=300, channel="chrome")
     context.page = browser.new_page()
+@fixture
+def logged_in_context(context):
+    address = "https://pprod.aldemia.fr/"
+    page = context.page
+
+    page.goto(address)
+    page.locator("#menu-item-29218").click()
+    page.get_by_text("vous déconnecter").click()
+    
+    page.goto(address)
+    page.locator("#menu-item-28912").click()
+
+    login = context.config.userdata["EMAIL"]
+    password = context.config.userdata["PASSWORD"]
+
+    page.locator("#rpress_user_login").fill(login)
+    page.locator("#rpress_user_pass").fill(password)
+    page.locator("#rpress_login_submit").click()
+    page.locator("#menu-item-28852").click()
 
 def before_all(context):
     use_fixture(playwright_browser_chrome, context)
+    use_fixture(logged_in_context, context)
 
 def before_feature(context, feature):
     features = (scenario for scenario in feature.scenarios if type(scenario) == ScenarioOutline and 'dynamic' in scenario.tags)
@@ -38,30 +58,17 @@ def before_feature(context, feature):
 
                 for article in order["articles"]:
                     articles.append(article["article"])
-                    # if articles == "":
-                    #     articles = article
-                    # else:
-                    #     articles = articles+";"+article
-                    # article_options = ""
+                
                     article_options = array_to_string(article["options"], ",")
-                    # for option in article["options"]:
-                    #     if article_options == "":
-                    #         article_options = option
-                    #     else:
-                    #         article_options = article_options+","+option
+                    
                     options.append(article_options)
-                    # if options == "":
-                    #     options = article_options
-                    # else:
-                    #     options = options+";"+article_options
+                    
                     quantity.append(str(article["quantity"]))
 
                 articles = array_to_string(articles, ";")
                 # articles = ";".join(articles)
                 options =  array_to_string(options, ";")
                 quantity = array_to_string(quantity, ";")   
-                #     n.cells = ['{}'.format(order["order_name"]), '{}'.format(article["article"]) , '{}'.format(article["options"]), '{}'.format(article["quantity"])]
-                #     example.table.rows.append(n)
                
                 n = Row(headings=headers, cells=['{}'.format(order["order_name"]),
                            '{}'.format(articles),
@@ -75,6 +82,17 @@ def before_feature(context, feature):
         # Print the data
         # print(data)      
 
+    if "Constructing an order" in feature.name:
+        page = context.page
+        page.on("dialog", lambda dialog: dialog.accept())
+        page.locator("div.rpress-price-holder:not(.rpress-grid-view-holder)").locator(f'a[data-title="12 wings"]').click()
+        page.locator("#nav-delivery-tab").click()
+            
+        page.locator('#rpress-delivery-hours').select_option(value="20:00")
+        page.locator("a.rpress-delivery-opt-update").click()
+        page.locator("a.submit-fooditem-button").click()
+        page.locator("a.rpress-remove-from-cart").click()
+
 def array_to_string (array, separator:str):
 
     # string_result = ""
@@ -85,4 +103,6 @@ def array_to_string (array, separator:str):
         # else:
         #     string_result = string_result+separator+item
     return string_result
-    
+
+
+        
